51191bd6bb971bc1a33cb8e9a2859bf42df4aa0c
Paul Dardeau <pauldardeau@me.com>
2013-06-25 14:41:27 -0500

    cleanup before github push

--- ApigeeiOSSDK/ApigeeiOSSDK/ApigeeCustomASLMessageKeys.h
+++
@@ -1,9 +0,0 @@
-//
-//  ApigeeCustomASLMessageKeys.h
-//  ApigeeAppMonitor
-//
-//  Created by jaminschubert on 10/9/12.
-//  Copyright (c) 2012 Apigee. All rights reserved.
-//
-
-#define kApigeeLogLevelASLMessageKey "kApigeeLogLevelASLMessageKey"
