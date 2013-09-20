105606fb208145489e04f9d4b391ff3fb063b380
Paul Dardeau <pauldardeau@me.com>
2013-09-20 18:19:14 -0500

    Removed auto-generated created by comment; updated copyright

--- samples/push/pushtest/main.m
+++ samples/push/pushtest/main.m
@@ -2,8 +2,7 @@
 //  main.m
 //  pushtest
 //
-//  Created by Scott Ganyo on 1/28/13.
-//  Copyright (c) 2013 Scott Ganyo. All rights reserved.
+//  Copyright (c) 2013 Apigee. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
