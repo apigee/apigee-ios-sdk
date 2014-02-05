27cce42f7d2f964485c67e6c1e586e8edad01606
Steven Traut <straut@apigee.com>
2014-02-05 15:07:49 -0500

    Adding code to check for an existing username.

--- samples/usersAndGroups/UsersAndGroups/UsersAddUserViewController.m
+++ samples/usersAndGroups/UsersAndGroups/UsersAddUserViewController.m
@@ -16,16 +16,49 @@
 
 @synthesize userName, name, email, password, delegate;
 
+ApigeeClient *apigeeClient;
+
 /**
  * Creates a new user in the application from information provided
  * through fields in the UI.
  */
 -(IBAction)createUser:(id)sender {
-    // Adds a user to the application through an SDK method.
-    [[self delegate] addUser:userName.text
-                        name:name.text
-                       email:email.text
-                    password:password.text];
+    
+    NSString *userNameValue = userName.text;
+    NSString *nameValue = name.text;
+    NSString *emailValue = email.text;
+    NSString *passwordValue = password.text;
+
+    // Create a query instance specifying the path that
+    // was requested for the new group.
+    ApigeeQuery *query = [[ApigeeQuery alloc] init];
+    [query addRequirement:[NSString stringWithFormat:@"username='%@'", userNameValue]];
+
+    
+    // Find out if there's already a user with this username. Some callback
+    // methods have been omitted for brevity.
+    [[apigeeClient dataClient] getUsers:query
+                      completionHandler:^(ApigeeClientResponse *usersResponse)
+    {
+        if ([usersResponse completedSuccessfully])
+        {
+            if ([usersResponse.response[@"entities"] count] > 0)
+            {
+                // If any entities were returned, it means there's a
+                // user with that username.
+                [self displayAlert:@"That username is taken. Please choose another."];
+            } else {
+                // If no entities, add a new user with an async SDK method.
+                [[self delegate] addUser:userNameValue
+                                    name:nameValue
+                                   email:emailValue
+                                password:passwordValue];
+            }
+        } else {
+            // Log (or display) a message.
+            ApigeeLogError(@"AddUser", @"Error while getting user data.");
+        }
+    }];
     
     // Dismisses this UI after the group is added.
     [self.navigationController popViewControllerAnimated:YES];
@@ -39,6 +72,20 @@
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+/**
+ * Displays an alert message.
+ */
+- (void)displayAlert:(NSString *)message
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Users and Groups"
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
 
 // The following are used in creating and configuring the view.
 
