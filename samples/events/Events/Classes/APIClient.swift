858837e1a139b6cde666c82b6b591dc3a0981558
Robert Walsh <rjwalsh1985@gmail.com>
2014-06-30 15:29:33 -0700

    Added the Events Sample

---
+++ samples/events/Events/Classes/APIClient.swift
@@ -0,0 +1,104 @@
+//
+//  APIClient.swift
+//  Events
+//
+//  Created by Robert Walsh on 6/23/14.
+//  Copyright (c) 2014 Apigee. All rights reserved.
+//
+
+import Foundation
+
+struct ApigeeClientOrgAndAppID {
+    static let organizationID = "rwalsh"
+    static let applicationID = "sdk.demo"
+}
+
+struct APIClientStaticStrings {
+    static let type = "type"
+    static let publicEventsCollection = "publicEvents"
+    static let privateEventsCollection = "privateEvents"
+    static let privateConnectionType = "private"
+    static let usersConnectorType = "users"
+    static let meConnectorID = "me"
+    static let entitiesCollectionKey = "entities"
+}
+
+class APIClient : NSObject {
+
+    let apigeeClient: ApigeeClient!
+
+    var dataClient: ApigeeDataClient {
+        return self.apigeeClient.dataClient()
+    }
+    var monitoringClient: ApigeeMonitoringClient {
+        return self.apigeeClient.monitoringClient()
+    }
+    var currentUser: ApigeeUser? {
+        return self.dataClient.getLoggedInUser()
+    }
+
+    init() {
+        apigeeClient = ApigeeClient(organizationId: ApigeeClientOrgAndAppID.organizationID, applicationId: ApigeeClientOrgAndAppID.applicationID)
+        super.init()
+    }
+
+    class func sharedClient() -> APIClient {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0
+            static var instance : APIClient? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = APIClient()
+        }
+        return Static.instance!
+    }
+
+    // MARK: Login/Logout user.
+    func logoutCurrentUser() {
+        if let user = self.currentUser {
+            self.dataClient.logOut(user.username)
+        }
+    }
+
+    func login(username: String?, password: String?, completion: ((Bool,String?) -> Void)?) {
+        self.dataClient.logInUser(username, password: password, completionHandler: { response in
+            if let completionBlock = completion {
+                completionBlock(response.completedSuccessfully(),response.error)
+            }
+        })
+    }
+
+    // MARK: Creating/Registering a new user.
+    func createUser(username: String?, fullName: String? ,email: String?, password: String?, completion: ((Bool,String?) -> Void)?) {
+        self.dataClient.addUser(username, email: email, name: fullName, password: password, completionHandler: {[weak self] response in
+            if response.completedSuccessfully() {
+                self?.login(username, password: password, completion:completion)
+            } else if let completionBlock = completion {
+                completionBlock(false,response.rawResponse)
+            }
+        })
+    }
+
+    // MARK: Grabbing Public/Private Events
+    func getPublicEvents(query: ApigeeQuery?) -> NSArray? {
+        if let eventQuery = query {
+            return self.dataClient.getCollection(APIClientStaticStrings.publicEventsCollection, usingQuery: query).list
+        } else {
+            return self.dataClient.getCollection(APIClientStaticStrings.publicEventsCollection).list
+        }
+    }
+
+    func getPrivateEvents(query: ApigeeQuery?) -> NSArray? {
+        var events: NSArray? = nil
+        if let collection = self.dataClient.getEntityConnections(APIClientStaticStrings.usersConnectorType, connectorID:APIClientStaticStrings.meConnectorID, connectionType: APIClientStaticStrings.privateConnectionType, query: query).response as? NSDictionary {
+            if let entities = collection[APIClientStaticStrings.entitiesCollectionKey] as? NSArray {
+                events = entities
+            }
+        }
+        return events
+    }
+
+    func getEvents(query: ApigeeQuery?) -> (publicEvents: NSArray?,privateEvents: NSArray?) {
+        return (self.getPublicEvents(query),self.getPrivateEvents(query))
+    }
+}
---
+++ samples/events/Events/Classes/Custom Views/APIEventCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  APIEventCollectionViewCell.swift
+//  Events
+//
+//  Created by Robert Walsh on 6/25/14.
+//  Copyright (c) 2014 Apigee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class APIEventCollectionViewCell : UICollectionViewCell {
+    @IBOutlet var eventNameLabel: UILabel
+    @IBOutlet var locationLabel: UILabel
+}
