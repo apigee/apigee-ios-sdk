acfb30dacd76ffbd7c672bddba46bc6198025857
Robert Walsh <rjwalsh1985@gmail.com>
2014-12-10 15:23:32 -0800

    Updating to fix testing

---
+++ source/Pods/Target Support Files/Pods-ApigeeiOSSDK IntegrationTests-PLCrashReporter/Pods-ApigeeiOSSDK IntegrationTests-PLCrashReporter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ApigeeiOSSDK_IntegrationTests_PLCrashReporter : NSObject
+@end
+@implementation PodsDummy_Pods_ApigeeiOSSDK_IntegrationTests_PLCrashReporter
+@end
---
+++ source/Pods/Target Support Files/Pods-ApigeeiOSSDK UnitTests-PLCrashReporter/Pods-ApigeeiOSSDK UnitTests-PLCrashReporter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ApigeeiOSSDK_UnitTests_PLCrashReporter : NSObject
+@end
+@implementation PodsDummy_Pods_ApigeeiOSSDK_UnitTests_PLCrashReporter
+@end
