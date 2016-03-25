0fa0e41ea67b404db1f62ffc9ecccff0052fa1ef
Robert Walsh <rjwalsh1985@gmail.com>
2016-03-25 09:19:55 -0500

    Updated to version 2.0.18

--- source/Classes/ApigeeClient.m
+++ source/Classes/ApigeeClient.m
@@ -22,9 +22,9 @@
 #import "ApigeeDefaultiOSLog.h"
 
 /*!
- @version 2.0.17
+ @version 2.0.18
  */
-static NSString* kSDKVersion = @"2.0.17";
+static NSString* kSDKVersion = @"2.0.18";
 
 
 @interface ApigeeClient ()
